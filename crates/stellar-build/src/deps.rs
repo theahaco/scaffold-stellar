aeb8560ae4613f1a9c1ee9ab26dc9e137a07fd2a
Pamphile Roy <23188539+tupui@users.noreply.github.com>
2025-10-07 19:16:08 +0200

    Update deps.rs
    Co-authored-by: Willem Wyndham <willem@wyndham.tech>

--- crates/stellar-build/src/deps.rs
+++ crates/stellar-build/src/deps.rs
@@ -142,7 +142,7 @@ pub fn all(manifest_path: &Path) -> Result<Vec<Package>, Error> {
 #[must_use]
 pub fn stellar_wasm_out_dir(target_dir: &Path) -> PathBuf {
     let network = std::env::var("STELLAR_NETWORK").unwrap_or_else(|_| "local".to_owned());
-    target_dir.join(format!("stellar/{network}"))
+    target_dir.join("stellar").join(network)
 }
 
 #[must_use]
