04df1f4adb920ad05acdd79b03e4c7dbac290297
Tupui <23188539+tupui@users.noreply.github.com>
2025-10-16 20:33:49 +0200

    Prod fixes

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://theahaco.github.io',
+  url: 'https://scaffoldstellar.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
