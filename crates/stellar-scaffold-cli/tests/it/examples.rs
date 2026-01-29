0645e9db29dd1031795057fa79b8800c8f389162
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2026-01-29 14:37:29 -0500

    Add integration tests for generating from oz examples

--- crates/stellar-scaffold-cli/tests/it/examples.rs
+++ crates/stellar-scaffold-cli/tests/it/examples.rs
@@ -29,7 +29,78 @@ use stellar_scaffold_test::{AssertExt, TestEnv, rpc_url};
 #[case::timelock("stellar/timelock")]
 #[case::token("stellar/token")]
 #[case::ttl("stellar/ttl")]
-fn test_adding_and_building_example_work(#[case] input: &str) {
+fn test_adding_and_building_soroban_examples(#[case] input: &str) {
+    TestEnv::from("soroban-init-boilerplate", |env| {
+        env.set_environments_toml(format!(
+            r#"
+[development]
+network = {{ rpc-url = "{}", network-passphrase = "Standalone Network ; February 2017" }}
+
+accounts = [
+    "alice",
+]
+[development.contracts]
+soroban_hello_world_contract.client = false
+soroban_increment_contract.client = false
+soroban_custom_types_contract.client = false
+soroban_auth_contract.client = false
+soroban_token_contract.client = false
+"#,
+            rpc_url()
+        ));
+
+        println!("starting test in directory {:?}", env.cwd);
+        eprintln!("test directory {:?}", env.cwd);
+
+        assert!(
+            env.cwd.join("contracts").is_dir(),
+            "no contracts directory found"
+        );
+        fs_extra::dir::remove(env.cwd.join("contracts"))
+            .expect("failed to remove contracts directory");
+
+        env.scaffold("generate")
+            .arg("contract")
+            .arg("--from")
+            .arg(input)
+            .arg("--output")
+            .arg(format!("{}/contracts/example", env.cwd.display()))
+            .assert()
+            .success()
+            .stdout_as_str();
+
+        // Run scaffold_build and assert success
+        env.scaffold_build("development", true).assert().success();
+    });
+}
+
+#[rstest]
+#[case::oz_fungible_allowlist("oz/fungible-allowlist")]
+#[case::oz_fungible_blocklist("oz/fungible-blocklist")]
+#[case::oz_fungible_capped("oz/fungible-capped")]
+// the following are commented out because they currently do not build from a freshly created project
+// this is because we need to add a few deps to the FE template that's being used
+// stellar-contract-utils, etc
+// i also wonder if instead of updating the soroban-boilterplate-init we should use the FE template direclty to really test things properly
+
+//  #[case::oz_fungible_merkle_airdrop("oz/fungible-merkle-airdrop")]❌
+//  #[case::oz_fungible_pausable("oz/fungible-pausable")] ❌
+#[case::oz_fungible_vault("oz/fungible-vault")]
+//  #[case::oz_merkle_voting("oz/merkle-voting")]❌
+//  #[case::oz_multisig("oz/multisig")] ❌ ?
+#[case::oz_nft_access_control("oz/nft-access-control")]
+#[case::oz_nft_consecutive("oz/nft-consecutive")]
+#[case::oz_nft_enumberable("oz/nft-enumerable")]
+#[case::oz_nft_royalties("oz/nft-royalties")]
+#[case::oz_nft_sequential_minting("oz/nft-sequential-minting")]
+#[case::oz_ownable("oz/ownable")]
+//  #[case::oz_pausable("oz/pausable")] ❌
+//  #[case::oz_rwa("oz/rwa")] ❌
+//  #[case::oz_sac_admin_generic("oz/sac-admin-generic")] ❌
+#[case::oz_sac_admin_wrapper("oz/sac-admin-wrapper")]
+//  #[case::oz_upgradeable("oz/upgradeable")] ❌
+
+fn test_adding_and_building_oz_examples(#[case] input: &str) {
     TestEnv::from("soroban-init-boilerplate", |env| {
         env.set_environments_toml(format!(
             r#"
