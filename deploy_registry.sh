e4f8c129c86e910fc859632259277d8f9fe7a0e7
ifropc <ifropc@protonmail.com>
2026-03-16 23:30:16 -0700

    feat: update deploy script

--- deploy_registry.sh
+++ deploy_registry.sh
@@ -3,12 +3,12 @@ set -e
 
 PATH=./target/bin:$PATH
 
-# ifndef STELLAR_NETWORK
-#    override STELLAR_NETWORK = local
-# endif
-
-VERIFED=$(sha256 -s verified)
-UNVERIFIED=$(sha256 -s unverified)
+# sha256 -s verified
+# echo -n verified | sha256sum
+VERIFED=1c34f88707b55e6104c4eb20e71ffa3d33e414b71ef689a15fad0640d0ac58cb
+# sha256 -s unverified
+# echo -n unverified | sha256sum
+UNVERIFIED=97b7e2db799e2b79e65f418b42a7d3054c95b2d3ab1dba243039597e44a38084
 ADMIN=theahaco
 ADDRESS=GAMPJROHOAW662FINQ4XQOY2ULX5IEGYXCI4SMZYE75EHQBR6PSTJG3M
 
@@ -18,14 +18,8 @@ stellar contract deploy --alias registry \
                         --salt $VERIFED \
                         -- \
                         --admin "$ADMIN" \
-                        --manager "\"$ADDRESS\""
-
-stellar contract deploy --alias registry \
-                        --wasm ./target/stellar/local/registry.wasm \
-                        --source "$ADMIN" \
-                        --salt $UNVERIFIED \
-                        -- \
-                        --admin "$ADMIN" 
+                        --manager "\"$ADDRESS\"" \
+                        --is-root true
 
 
 registry="stellar contract invoke --id registry --"
